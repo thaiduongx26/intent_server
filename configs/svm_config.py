21d41cc9b3a379bf064c8951fa469c5578bdf335
howard <thaiduongx26@gmail.com>
2020-08-27 10:49:00 +0700

    update new model

--- configs/svm_config.py
+++ configs/svm_config.py
@@ -1,4 +1,4 @@
 class Config_SVM():
     def __init__(self):
-        self.VECTORIZER_PATH = 'storages/vec_300_1_4.pkl'
-        self.MODEL_PATH = 'storages/svm_300_1_4.pkl'
+        self.VECTORIZER_PATH = 'storages/vec_400_100_50.pkl'
+        self.MODEL_PATH = 'storages/svm_400_100_50.pkl'
